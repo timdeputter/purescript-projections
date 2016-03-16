79b947d6b48a4b1e9b069123aae195212176d27b
Tim de Putter <tim.de.putter83@googlemail.com>
2016-03-16 21:19:21 +0100

    fixed Main file

--- src/Main.purs
+++ src/Main.purs
@@ -2,17 +2,17 @@ module Main where
 
 import Prelude (Unit, (+), ($))
 import Control.Monad.Eff (Eff)
-import Purejections (EventFold, when, (+++), runProjection)
+import Projections (Projection, when, (+++), runProjection)
 
 -- Example state type
 type State = {count :: Int}
 
-type NewStat = {event :: String}
+type TypA = {event :: String}
 
 handlerA :: State -> TypA -> State
 handlerA s e = {count: s.count+1}
 
-handlerB :: State -> TypB -> State
+handlerB :: State -> TypA -> State
 handlerB s e = {count: s.count+1}
 
 main :: forall t9. Eff ( eventFold :: Projection | t9) Unit
