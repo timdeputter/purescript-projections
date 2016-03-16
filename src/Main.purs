f16f02af031a073d0787e9181a4d014c9433d98b
Tim de Putter <tim.de.putter83@googlemail.com>
2016-03-16 22:00:16 +0100

    changed example

--- src/Main.purs
+++ src/Main.purs
@@ -16,4 +16,4 @@ handlerB :: State -> TypA -> State
 handlerB s e = {count: s.count+1}
 
 main :: forall t9. Eff ( eventFold :: Projection | t9) Unit
-main = runProjection "Figo" {count:0} $ when "Hello" handlerA +++ when "Figo" handlerB
+main = runProjection "$stats-127.0.0.1:2113" {count:0} $ when "$statsCollected" handlerA +++ when "Figo" handlerB
